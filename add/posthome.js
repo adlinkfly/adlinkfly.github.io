350dce38bd426feb60b5d767175489531cd2236a
DrWong <bookunisite@gmail.com>
2016-12-21 23:29:43 -0400

    test
    test

--- add/posthome.js
+++ add/posthome.js
@@ -1,3 +1,8 @@
+/* AddThis */
+var addthisScript = document.createElement('script');
+addthisScript.setAttribute('src', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-583e42fbd9e86109');
+document.body.appendChild(addthisScript);
+
 /* Google Analytics */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -13,10 +18,3 @@ var sc_security="74f5292e";
 var sc_script=document.createElement('script');
 sc_script.setAttribute('src', '//statcounter.com/counter/counter.js');
 document.body.appendChild(sc_script);
-
-/* AddThis */
-setTimeout(function(){
-  var addthisScript = document.createElement('script');
-  addthisScript.setAttribute('src', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-583e42fbd9e86109');
-  document.body.appendChild(addthisScript);
-},1500);
